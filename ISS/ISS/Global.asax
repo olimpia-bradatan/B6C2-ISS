fd7137adf90f43cb440f66f5eccdf00be0f79acf
bradatanolimpia <bradatan.olimpia@yahoo.com>
2018-03-04 12:20:14 +0200

    start

---
+++ ISS/ISS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ISS.MvcApplication" Language="C#" %>
---
+++ ISS/ISS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ISS.Startup))]
+namespace ISS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
